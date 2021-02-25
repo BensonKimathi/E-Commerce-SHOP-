167b956f68b1d9d3db8dead3179addfb53e1e2ff
Benson Kimathi <37022584+BensonKimathi@users.noreply.github.com>
2021-02-25 08:12:22 +0300

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "odipoint");
+define("TITLE", "Odipoint");
+define("KEYWORDS", "Odipoint, Online, Shopping, Electronics");
+?>
